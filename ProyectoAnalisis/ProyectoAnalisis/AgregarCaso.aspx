9e120855a21fe529c01fbae0c18072fff4433571
powerpanchin <powerpanchin@gmail.com>
2015-08-20 23:48:49 -0600

    Version 11:45 pm 20/08/2015
    Se agrego el script de la base de datos, y se creo la conexión de la
    base de datos en las paginas, y se comprobó que la conexión funciona
    correctamente.

--- ProyectoAnalisis/ProyectoAnalisis/AgregarCaso.aspx
+++ ProyectoAnalisis/ProyectoAnalisis/AgregarCaso.aspx
@@ -2,4 +2,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+    </p>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Prueba" />
+    <br />
+    <br />
 </asp:Content>
--- ProyectoAnalisis/ProyectoAnalisis/AgregarCaso.aspx.cs
+++ ProyectoAnalisis/ProyectoAnalisis/AgregarCaso.aspx.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
 
 namespace ProyectoAnalisis
 {
     public partial class AgregarCaso : System.Web.UI.Page
     {
+        MySqlConnection connection = new MySqlConnection();
+        String connectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void conectarBase()
+        {
+            try
+            {
+
+                connectionString = "server=127.0.0.1;uid=root;database=analisis1;port=3306;pwd=12345;";
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                MessageBox.Show("Conexion Exitosa",":)");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("El servidor de base de datos esta caido!", "Servidor No Disponible");
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            conectarBase();
         }
     }
 }
